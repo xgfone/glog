e7f743a1b4eb54c7e88704cd00f928b676ad284f
xgfone <xgfone@126.com>
2018-10-28 11:40:43 +0800

    add the method Depth for the interface Logger

--- logger.go
+++ logger.go
@@ -22,8 +22,26 @@ import (
 // ErrPanic will be used when firing a PANIC level log.
 var ErrPanic = fmt.Errorf("the panic level log")
 
+// DefaultLoggerDepth is the depth for the default implementing logger.
+const DefaultLoggerDepth = 2
+
 // Logger is a logger interface.
 type Logger interface {
+	// stackDepth is the calling depth of the logger, which will be passed to
+	// the encoder. The default depth is the global variable DefaultLoggerDepth.
+	//
+	// It should be used typically when you wrap the logger. For example,
+	//
+	//   logger := miss.New(miss.KvTextEncoder(os.Stdout))
+	//   logger = logger.Depth(miss.DefaultLoggerDepth + 1)
+	//
+	//   func Debug(m string, args ...interface{}) { logger.Debug(m, args...) }
+	//   func Info(m string, args ...interface{}) { logger.Debug(m, args...) }
+	//   func Warn(m string, args ...interface{}) { logger.Debug(m, args...) }
+	//   ...
+	//
+	Depth(stackDepth int) Logger
+
 	Level(level Level) Logger
 	Encoder(encoder Encoder) Logger
 	Cxt(ctxs ...interface{}) Logger
@@ -41,6 +59,8 @@ type logger struct {
 	enc Encoder
 	lvl Level
 	ctx []interface{}
+
+	depth int
 }
 
 // New returns a new Logger.
@@ -49,6 +69,8 @@ func New(encoder Encoder) Logger {
 		lvl: TRACE,
 		enc: encoder,
 		ctx: make([]interface{}, 0),
+
+		depth: DefaultLoggerDepth,
 	}
 }
 
@@ -58,6 +80,12 @@ func newLogger(l logger) logger {
 		ctx: l.ctx,
 		lvl: l.lvl,
 	}
+}
+
+func (l logger) Depth(depth int) Logger {
+	log := newLogger(l)
+	log.depth = depth
+	return log
 }
 
 func (l logger) Level(level Level) Logger {
@@ -82,7 +110,7 @@ func (l logger) log(level Level, msg string, args []interface{}) (err error) {
 	if level < l.lvl {
 		return nil
 	}
-	err = l.enc.Encode(2, level, msg, args, l.ctx)
+	err = l.enc.Encode(l.depth, level, msg, args, l.ctx)
 
 	switch level {
 	case PANIC:
